3a7f43688c5930adde35857d94bf24468784b3e2
Muhammed Ashraf <mohamed8ashraf2@gmail.com>
2024-08-28 16:16:40 +0300

    Add EasyCode ignore file and initial setup for letters learning feature
    - Created `.vscode/easycode.ignore` to specify files and directories to be ignored by EasyCode, including `node_modules/`, `dist/`, `vendor/`, and others.
    - Introduced `letters_learning_view.dart` and `letters_learning_view_body.dart` under `lib/features/letters_learning/presentation/` to lay the groundwork for the letters learning feature in the application. These files include basic structure and imports necessary for the feature's presentation layer.

---
+++ lib/features/letters_learning/presentation/views/letters_learning_view.dart
@@ -0,0 +1,11 @@
+import 'package:arabic_iti/features/letters_learning/presentation/widgets/letters_learning_view_body.dart';
+import 'package:flutter/material.dart';
+
+class LettersLearningView extends StatelessWidget {
+  const LettersLearningView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LettersLearningViewBody();
+  }
+}
