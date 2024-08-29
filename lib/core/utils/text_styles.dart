863cf458ea36547caeb799b0c15894e466e1b85a
Muhammed Ashraf <mohamed8ashraf2@gmail.com>
2024-08-29 12:04:21 +0300

    Add text styles utility class for consistent typography
    This commit introduces a new file, `text_styles.dart`, within the `lib/core/utils` directory. The file defines an abstract class `AppTextStyles` that encapsulates a set of reusable text styles. These styles are designed to ensure consistent typography across the application, promoting a unified user experience.
    
    The `AppTextStyles` class includes the following static TextStyle constants:
    - `bold20White`: A bold text style with a font size of 20 and white color.
    - `bold20`: A bold text style with a font size of 20.
    - `bold18`: A bold text style with a font size of 18.
    - `bold16`: A bold text style with a font size of 16.
    
    This utility class will simplify the management and application of text styles throughout the project, reducing redundancy and enhancing maintainability.

---
+++ lib/core/utils/text_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const TextStyle bold20White = TextStyle(
+    fontSize: 20,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+}
